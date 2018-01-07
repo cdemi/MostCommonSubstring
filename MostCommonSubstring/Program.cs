ae14ea6ab58bfad2b10abd3547c9adeff9b35cb9
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-01-07 16:28:38 +0100

    Solution

---
+++ MostCommonSubstring/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MostCommonSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Data
+            Console.WriteLine(MostCommonSubstring(5, 2, 4, 26, "abcde"));
+        }
+
+        private static int MostCommonSubstring(int n, int k, int l, int m, string str)
+        {
+            Dictionary<string, int> substrings = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+
+            for (int i = 0; i < n; i++)
+            {
+                int maxBounds;
+                if (k < n - i)
+                    maxBounds = k;
+                else
+                    maxBounds = n - 1;
+
+                for (int j = k; j <= maxBounds; j++)
+                {
+                    string substring = str.Substring(i, j);
+                    int distinctCharacters = substring.Distinct().Count();
+
+                    if (substring.Length >= k && substring.Length <= l && distinctCharacters <= m)
+                    {
+                        if (substrings.ContainsKey(substring))
+                            substrings[substring]++;
+                        else
+                            substrings.Add(substring, 1);
+                    }
+                }
+            }
+
+            return substrings.Max(s => s.Value);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# MostCommonSubstring
-My solution to the Most Common Substring problem
+# Most Common Substring
+My solution to the Most Common Substring problem.
+
+## Problem
+Given an input string of size `N`, return the number of occurrences of the most common substring between lengths `K` and `K`
+
+## Input
+ * The first line contains an integer `N`.
+ * The second line contains three space separated integers, `K` `L` `M`.
+ * The third line contains the string of length `N`.
+ 
+### Sample Input
+ ```
+ 5
+ 2 4 26
+ abcde
+ ``` 
+
+## Output
+### Sample Output
+ ```
+ 1
+ ```
